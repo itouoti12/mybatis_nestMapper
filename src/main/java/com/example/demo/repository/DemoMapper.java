03c3b4811f6b86f2b121534e1401a44d7a69e729
itouoti <white.sleipnir.jazz@gmail.com>
2019-06-22 12:39:39 +0900

    不要なインポートの削除

--- src/main/java/com/example/demo/repository/DemoMapper.java
+++ src/main/java/com/example/demo/repository/DemoMapper.java
@@ -7,7 +7,6 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.mapping.FetchType;
 
 import com.example.demo.entity.Address;
 import com.example.demo.entity.City;
